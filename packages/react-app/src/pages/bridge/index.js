baa62decfef9c569803d9a74548d1c21969a0934
willcodex <willcodexyz@amail.com>
2024-06-08 23:26:06 +0800

    feat: query source and target chain account balance

--- packages/react-app/src/pages/bridge/index.js
+++ packages/react-app/src/pages/bridge/index.js
@@ -166,14 +166,14 @@ const bridgeConfig = {
   sepolia: {
     address: addresses.depositL1,
     chainId: Sepolia.chainId,
-    text: 'Sepolia',
+    text: 'Sepolia Layer 1',
     target_text: 'Adventure Layer'
   },
   adventure: {
     address: addresses.depositL2,
     chainId: AdventureLayer.chainId,
     text: 'Adventure Layer',
-    target_text: 'Sepolia'
+    target_text: 'Sepolia Layer 1'
   },
 }
 
@@ -200,6 +200,7 @@ const BridgeIndex = () => {
   const [chainState, setChainState] = React.useState(addresses.depositL1);
   const [selectSource, setSelectSource] = React.useState("sepolia");
   const [targetChainName, setTargetChainName] = React.useState("Adventure Layer");
+  const [sourceChainName, setSourceChainName] = React.useState("Sepolia Layer 1");
 
   const handleChainChange = (event) => {
     // console.log(event)
@@ -207,7 +208,21 @@ const BridgeIndex = () => {
     const chain = bridgeConfig[event.target.value]
     setChainState(chain.address);
     setTargetChainName(chain.target_text);
+    setSourceChainName(chain.text);
   };
+
+  const handleSwitchChain = (event) => {
+    let source = 'sepolia'
+    if (selectSource === 'sepolia') {
+      source = 'adventure'
+    }
+
+    setSelectSource(source);
+    const chain = bridgeConfig[source]
+    setChainState(chain.address);
+    setTargetChainName(chain.target_text);
+    setSourceChainName(chain.text);
+  }
 
   const gasPriceGwei = '15'
   const l1Web3 = new Web3(Sepolia.rpcUrl)
@@ -220,18 +235,41 @@ const BridgeIndex = () => {
     l2: l2BalanceAmount,
   })
 
-  const reloadAccountBalance = () => {
-    if (account && library) {
-      library.getBalance(account).then((val) => {
-        // ethers.utils.formatEther();
-        const l1BalanceAmount = new Decimal(val.toString()).div(1000000000000000000).toFixed(5);
-        setAccountBalance({
-          ...accountBalance,
-          l1: l1BalanceAmount,
-        })
+  const reloadAccountBalance = async () => {
+    // if (account && library) {
+    //   library.getBalance(account).then((val) => {
+    //     // ethers.utils.formatEther();
+    //     const l1BalanceAmount = new Decimal(val.toString()).div(1000000000000000000).toFixed(5);
+    //     setAccountBalance({
+    //       ...accountBalance,
+    //       l1: l1BalanceAmount,
+    //     })
+    //   })
+    // }
+    if (account) {
+      try {
+        const l1Balance = await l1Web3.eth.getBalance(account)
+        console.log('=============', l1Balance)
+        l1BalanceAmount = new Decimal(l1Balance.toString()).div(1000000000000000000).toFixed(5)
+        // ethers.utils.formatEther(l1Balance)
+      } catch (err) {}
+      
+      try {
+        const l2Balance = await l2Web3.eth.getBalance(account)
+        console.log('=============', l2Balance)
+        l2BalanceAmount = new Decimal(l2Balance.toString()).div(1000000000000000000).toFixed(5)
+        // ethers.utils.formatEther(l2Balance)
+      } catch (err) {}
+      console.log('=============', {
+        ...accountBalance,
+        l1: l1BalanceAmount,
+        l2: l2BalanceAmount,
       })
-      // const l2Balance = l2Web3.getBalance(account)
-      // l2BalanceAmount = ethers.utils.formatEther(l2Balance);
+      setAccountBalance({
+        ...accountBalance,
+        l1: l1BalanceAmount,
+        l2: l2BalanceAmount,
+      })
     }
   }
   useEffect(() => {
@@ -324,7 +362,7 @@ const BridgeIndex = () => {
                   <div className='item1'>From</div>
                   <div className='item2' style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
                     <img src={eth_logo} alt='background' style={{ marginLeft: '13px', width: '22px', height: '22px' }} />
-                    <div className='item3'>Sepolia  Layer 1</div>
+                    <div className='item3'>{sourceChainName}</div>
                   </div>
                   {/* <div className='select'>
                     <FormControl sx={{ marginBottom: 1, Width: 159 }} size="small">
@@ -344,7 +382,7 @@ const BridgeIndex = () => {
                 <div className='send_box'>
                   <div className='send_title'>
                     <div className='send_txt'>Send</div>
-                    <div className='send_txt'>Max: {accountBalance.l1} ETH</div>
+                    <div className='send_txt'>Max: { selectSource == 'sepolia' ? accountBalance.l1 : accountBalance.l2} ETH</div>
                   </div>
                   {/* <FormControl sx={{ width: '100%', color: '#fff' }} variant="outlined"> */}
                   {/* <OutlinedInput
@@ -388,13 +426,15 @@ const BridgeIndex = () => {
                 </div>
               </div>
               <div className='trans_box'>
-                <img src={trans_log} alt='trans' />
+                <img src={trans_log} style={{
+                  cursor: 'pointer',
+                }} alt='trans' onClick={handleSwitchChain} />
               </div>
               <div className='to_box'>
                 <div className='to_1'>To</div>
                 <div className='to_2' style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
                   <img src={adv_logo} alt='background' style={{ marginLeft: '13px', width: '22px', height: '22px' }} />
-                  <div className='to_3' >Adventure Layer</div>
+                  <div className='to_3' >{targetChainName}</div>
                 </div>
               </div>
 
