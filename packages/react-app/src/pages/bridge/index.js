eb1d867875c76584efdbf88365c5748f4edbbdd1
nolancode1989 <nolancode1989@gmail.com>
2024-08-13 22:08:34 +0800

    Update index.js

--- packages/react-app/src/pages/bridge/index.js
+++ packages/react-app/src/pages/bridge/index.js
@@ -86,7 +86,7 @@ function ResponsiveAppBar() {
       <div className='menu'>
         <a className='menuItem' href="https://faucet-devnet.adventurelayer.xyz" target="_blank" rel="noopener noreferrer">Faucet</a>
         <a className='menuItem' href="https://bridge-devnet.adventurelayer.xyz" target="_blank" rel="noopener noreferrer">Bridge</a>
-        <a className='menuItem' href="https://docs-devnet.adventurelayer.xyz" target="_blank" rel="noopener noreferrer">Doc</a>
+        <a className='menuItem' href="https://docs.adventurelayer.xyz" target="_blank" rel="noopener noreferrer">Doc</a>
       </div>
       {/* <div className='connect'>
       <div className='Button'>Connect</div>
